e1ca693cb4131b1ab0395b211696543ac4016ff7
Nicolas Lucentini <nlucenti@everis.com>
2020-02-25 20:56:42 -0300

    HIce cambios varios y cambie de carpeta el .git para ever que pasa

---
+++ Assets/Scripts/Managers/BuildingSpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BuildingSpawnPoint : MonoBehaviour
+{
+    public int level = 1;
+    
+    public void AddLevel() {
+        level++;    
+    }
+    public bool HasChilds() {
+        return transform.childCount != 0;
+    }
+    public GameObject GetChild() {
+        if (HasChilds())
+            return transform.GetChild(0).gameObject;
+        else return null;
+    }
+    public void ResetSpawn() {
+        level = 1;
+        if (GetChild() != null)
+            Destroy(GetChild());
+    }
+}
